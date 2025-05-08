2bfb83c22c53190e4c8f82c81098192d419e02c2
Albiona Leka <albionnalekaa@gmail.com>
2025-05-09 00:44:46 +0200

    Signup Component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,6 +4,7 @@ import InvoiceForm from "./InvoiceForm";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleForm } from "../store/slice";
 import Details from "./Details";
+import Signup from "./Signup";
 
 const Content = () => {
     const dispatch = useDispatch();
@@ -19,6 +20,8 @@ const Content = () => {
 
     return (
         <div className="w-full bg-slate-900 text-white min-h-screen p-1 md:p-4">
+            <Signup />
+
             <div className="mx-auto max-w-5xl py-12 px-1 md:px-4">
                 <Header onNewInvoice={handleCreate} onInfo={handleInfo} />
 
---
+++ src/components/Signup.jsx
@@ -0,0 +1,83 @@
+import { Link, Route, Routes } from "react-router-dom";
+import SignupImage from "../assets/signup.svg";
+import { ArrowDownToDotIcon } from "lucide-react";
+import Invoices from "./Invoices";
+import axios from "axios";
+import { useState } from "react";
+
+const Signup = () => {
+    const [formData, setFormData] = useState(() => {
+        return {
+            name: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+            profilePicture: null,
+        }
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        try {
+            if (formData.password !== formData.confirmPassword) {
+                alert("Passwords do not match!");
+                return;
+            }
+
+            const url = "http:localhost:3000/api/user/signup";
+            const user = {
+                name: formData.name,
+                email: formData.email,
+                password: formData.password
+            }
+            const {data: res} = axios.post('http://localhost:3000/api/user/signup', user);
+            console.log(res);
+        } catch (error) {
+            console.error("Error signing up:", error);
+            alert("Error signing up. Please try again.");
+        }
+    }
+
+    return (
+        <div className="bg-slate-800 rounded-lg shadow-lg p-4 md:p-8 h-lvh flex flex-items items-center justify-center space-y-4 md:space-y-8">
+            <div className="hidden md:block mb-4 w-full">
+                <img src={SignupImage} alt="Signup illustration" />
+            </div>
+            
+            <div className="mb-4 w-full">
+                <h1 className="text-2xl font-bold text-center mb-4 text-rose-600">Sign Up</h1>
+                <form className="flex flex-col items-center">
+                    <input type="text" placeholder="Full Name" className="md:w-80 mb-4 px-6 py-2 rounded-full border"
+                        required onChange={e => setFormData({...formData, name: e.target.value})} />
+                    <input type="email" placeholder="Email" className="md:w-80 mb-4 px-6 py-2 rounded-full border"
+                        required onChange={e => setFormData({...formData, email: e.target.value})} />
+                    <input type="password" placeholder="Password" className="md:w-80 mb-4 px-6 py-2 rounded-full border"
+                        required onChange={e => setFormData({...formData, password: e.target.value})} />
+                    <input type="password" placeholder="Confirm Password" className="md:w-80 mb-4 px-6 py-2 rounded-full border" 
+                        required onChange={e => setFormData({...formData, confirmPassword: e.target.value})} />
+                    <div className="flex justify-start items-center mb-4">
+                        <ArrowDownToDotIcon className="w-6 h-6 text-rose-500" />
+                        <label className="text-sm text-slate-400">Upload your profile picture</label>
+                    </div>
+                    <input
+                        type="file"
+                        onChange={e => setFormData({...formData, profilePicture: e.target.files[0]})}
+                        className="md:w-80 mb-4 file:mr-4 file:rounded-full file:border-0 file:bg-rose-500 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-rose-700 hover:file:bg-rose-100 dark:file:bg-rose-600 dark:file:text-rose-100 dark:hover:file:bg-rose-500"
+                    />
+                    <button type="submit" className="bg-rose-300 text-white py-2 px-4 rounded-full" onClick={handleSubmit}>Sign Up</button>
+                </form>
+                <p className="text-center mt-4">Already have an account? 
+                    <Link to="/login">Login</Link>
+                </p> 
+            </div>
+
+            <Routes className="hidden md:block mb-4 w-full">
+                <Route path="/login" element={<Invoices />} />
+            </Routes>
+            
+        </div>
+    )
+}
+
+export default Signup;
